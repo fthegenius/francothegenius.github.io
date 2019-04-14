b5aee150bd20aae75bea9da9492340491e50c2da
fthegenius <francothegenius@gmail.com>
2019-04-14 13:25:22 -0500

    form php

---
+++ contactform.php
@@ -0,0 +1,15 @@
+<?php
+
+if (isset($_POST['submit'])) {
+    $name = $_POST['name'];
+    $subject = $_POST['subject'];
+    $mailFrom = $_POST['e-mail'];
+    $message = $_POST['message'];
+
+    $mailTo = "fthegenius@outlook.com";
+    $headers = "From: My Website ".$mailFrom;
+    $txt = "You´ve received an e-mail from ".$name.".\n\n".$message;
+
+    mail($mailTo, $subject, $txt, $headers);
+    header("Location: contact.html?mailsend");
+}
