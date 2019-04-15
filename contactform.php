af2c99369424b73ad8fe6e7a79bf93e27b0ef3e3
fthegenius <francothegenius@gmail.com>
2019-04-14 22:38:46 -0500

    webhost, contact form, dissapear 000webhost, required contact form

--- contactform.php
+++ contactform.php
@@ -1,15 +1,16 @@
 <?php
 
-if (isset($_POST['submit'])) {
-    $name = $_POST['name'];
-    $subject = $_POST['subject'];
-    $mailFrom = $_POST['e-mail'];
-    $message = $_POST['message'];
+if(isset($_POST['submit'])) {
+$name = $_POST['name'];
+$subject = $_POST['subject'];
+$mailFrom = $_POST['e-mail'];
+$message = $_POST['message'];
 
-    $mailTo = "fthegenius@outlook.com";
-    $headers = "From: My Website ".$mailFrom;
-    $txt = "You´ve received an e-mail from ".$name.".\n\n".$message;
+$mailTo = "fthegenius@outlook.com";
+$headers = "From: My Website ".$mailFrom;
+$txt = "You have received an e-mail from ".$name.".\n\n".$message;
 
-    mail($mailTo, $subject, $txt, $headers);
-    header("Location: contact.html?mailsend");
+mail($mailTo, $subject, $txt, $headers);
+header("Location: contact.html");
 }
+?>
